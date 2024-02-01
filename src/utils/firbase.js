0f019f5f81fc9b0396d91785d036e7322ed4b836
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2024-02-01 12:18:29 +0530

    deploy updated

--- src/utils/firbase.js
+++ src/utils/firbase.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBz5Tdjr5pKHa0UZXIhbEwVCBxxZ5cdxs",
-  authDomain: "gptflix-1d120.firebaseapp.com",
-  projectId: "gptflix-1d120",
-  storageBucket: "gptflix-1d120.appspot.com",
-  messagingSenderId: "75470581739",
-  appId: "1:75470581739:web:c504a89b31c111f19333ae",
-  measurementId: "G-C2J93RJW84"
+  apiKey: "AIzaSyAmDtUYgsImYx_2-V8_Q72OzUKR__rZiog",
+  authDomain: "gptflix-564e5.firebaseapp.com",
+  projectId: "gptflix-564e5",
+  storageBucket: "gptflix-564e5.appspot.com",
+  messagingSenderId: "197684122336",
+  appId: "1:197684122336:web:f1cf2f3f175573edc83685",
+  measurementId: "G-FN50C0HK41"
 };
 
 // Initialize Firebase
